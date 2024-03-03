0d20b87d79f30fc8823efaddf615cc5a176e8a4f
NoprauxX12 <juanselizca@outlook.com>
2024-03-03 14:11:54 -0500

    Login in page

--- views/src/screens/login/loginScreen.jsx
+++ views/src/screens/login/loginScreen.jsx
@@ -1,11 +1,16 @@
 import MiddleLogoContainer from "../../includes/middleLogoContainer";
+import FormularioLogIn from "../../includes/logInForm";
+import "../../styles/register.css";
+import React from "react";
 
 function LoginScreen() {
     
     return (
         <div>
             <MiddleLogoContainer/>
-            <div className="mitad div__form"></div>
+            <div className="mitad div__form">
+                <FormularioLogIn/>
+            </div>
         </div>
     )
 }
